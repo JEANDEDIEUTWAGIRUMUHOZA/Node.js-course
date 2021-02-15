a1fbfd3f4059d43eceb11eb18dfb6ff6924d0975
jeandedieu.twagirumuhoza <jeandedieu.twagirumuhoza@gmail.com>
2021-02-15 22:30:07 +0100

    added pug template engine

--- DynamicContentAndTemplatingEngines/server.js
+++ DynamicContentAndTemplatingEngines/server.js
@@ -75,6 +75,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
