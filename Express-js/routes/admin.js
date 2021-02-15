655a71496d2c9a3130cf9230627ba73ef2d07807
jeandedieu.twagirumuhoza <jeandedieu.twagirumuhoza@gmail.com>
2021-02-15 19:30:28 +0100

    Using helper function fir Navigation

--- Express-js/routes/admin.js
+++ Express-js/routes/admin.js
@@ -2,14 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
-//point to our paths file, see util directory/path.js
 const rootDir = require('../util/path');
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
